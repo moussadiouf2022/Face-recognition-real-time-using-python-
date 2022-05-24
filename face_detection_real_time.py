6fd2ce20d61ecf9ccf1ebbfa9f9ca684d59e5b6d
DIOUF <moussa.diouf2022@yahoo.com>
2022-05-25 00:47:01 +0200

    Version 3

--- face_detection_real_time.py
+++ face_detection_real_time.py
@@ -9,7 +9,7 @@ while True:
     boolean, frame = capture.read()
     if boolean == True:
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2) 
+        coordinate_list = pretrained_model.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=4) 
         
         # drawing rectangle in frame
         for (x,y,w,h) in coordinate_list:
@@ -19,7 +19,7 @@ while True:
         cv.imshow("Live Face Detection", frame)
         
         # condition to break out of while loop
-        if cv.waitKey(20) == ord(' '):
+        if cv.waitKey(20) == ord(' '): 
             break
     
         
